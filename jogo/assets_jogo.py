3bd8c98de16b028c93ef1aac9682d5f8e7c146d6
laupontiroli <laurapm@al.insper.edu.br>
2023-04-20 15:44:12 -0300

    removi monstro e fundo infinito que mexe

--- jogo/assets_jogo.py
+++ jogo/assets_jogo.py
@@ -1,6 +1,7 @@
 import pygame 
 
 class Player(pygame.sprite.Sprite):
+
     def __init__(self,window):
         pygame.sprite.Sprite.__init__(self)
         self.window=window
@@ -15,40 +16,43 @@ class Player(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.x=0
         self.rect.y=540
+        self.clock=pygame.time.Clock()
+        self.FPS=10
 
     def update(self):
+        self.clock.tick(self.FPS)
         for evento in pygame.event.get():
-
-
             if evento.type==pygame.QUIT:
                 return -1 
-            
         tecla = pygame.key.get_pressed()
-        if tecla[pygame.K_d]:
+        if tecla[pygame.K_d] or tecla[pygame.K_RIGHT]:
             self.indice_img=(self.indice_img+1)%len(self.images_animation)
             image=pygame.image.load(self.images_animation[self.indice_img])
             self.image=pygame.transform.scale(image, (60,60))
-
+            
     def desenha(self):
         self.window.blit(self.image,self.rect)
         pygame.display.update()
 
 class Biscoitos(pygame.sprite.Sprite):
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         pass
 
 class Nuvem(pygame.sprite.Sprite):
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
 
 class Monstro(pygame.sprite.Sprite):
+
     def __init__(self,window):
         pygame.sprite.Sprite.__init__(self)
         self.window=window
         self.indice_img=0
-        self.images_animation=['jogo/Assets_jogo/snow_monster/snow_monster_4_direita.png','jogo/Assets_jogo/snow_monster/snow_monster_4_esquerda.png','jogo/Assets_jogo/snow_monster/snow_monster_3_direita.png','jogo/Assets_jogo/snow_monster/snow_monster_3_esquerda.png']
+        self.images_animation=[]
         image=pygame.image.load(self.images_animation[self.indice_img])
         self.image=pygame.transform.scale(image, (80,80))
         self.vidas=1
@@ -56,13 +60,19 @@ class Monstro(pygame.sprite.Sprite):
         self.radius=(self.h)/2
         self.last_updated=0
         self.rect=self.image.get_rect()
-        self.rect.x=200
-        self.rect.y=540
+        self.rect.x=500
+        self.rect.y=520
 
     def update(self):
-        self.indice_img=(self.indice_img+1)%len(self.images_animation)
-        image=pygame.image.load(self.images_animation[self.indice_img])
-        self.image=pygame.transform.scale(image, (80,80))
+        for evento in pygame.event.get():
+            if evento.type==pygame.QUIT:
+                return -1 
+        tecla = pygame.key.get_pressed()
+        if tecla[pygame.K_d] or tecla[pygame.K_RIGHT]:
+            self.indice_img=(self.indice_img+1)%len(self.images_animation)
+            image=pygame.image.load(self.images_animation[self.indice_img])
+            self.image=pygame.transform.scale(image, (80,80))
+        
         
     def desenha(self):
         self.window.blit(self.image,self.rect)
--- jogo/jogo_GM.py
+++ jogo/jogo_GM.py
@@ -1,23 +1,30 @@
 import pygame 
 from assets_jogo import * 
+import math
 
 class Tela_Jogo():
     
     def __init__(self,window):
         self.window=window
-        fundo=pygame.image.load('jogo/Assets_jogo/img_fundo.png')
+        fundo=pygame.image.load('jogo/Assets_jogo/img_fundo.png'). convert()
         self.fundo=pygame.transform.scale(fundo, (800, 750))
         self.player=Player(self.window)
-        self.monstro=Monstro(self.window)
+        # self.monstro=Monstro(self.window)
+        self.tiles=math.ceil(self.window.get_width()/self.fundo.get_width())+1
+        self.scroll=0
+        
 
     def atualiza(self):
-        if self.player.update() == -1 or self.monstro.update() == -1:
+        if self.player.update() == -1:
             return -1
         return self  
     
     def desenha(self):
-        self.window.blit(self.fundo,(0,0))
-        self.monstro.desenha()
+        if abs(self.scroll)>self.fundo.get_width():
+            self.scroll=0
+        else: self.scroll-=5
+        for i in range(self.tiles):
+            self.window.blit(self.fundo,(i*self.fundo.get_width()+self.scroll,0))
         self.player.desenha()
         pygame.display.update()
 
@@ -26,10 +33,11 @@ class Jogo:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('Cookie Rush')
-        self.largura_tela=800
+        self.largura_tela=1300
         self.altura_tela=750
         self.window=pygame.display.set_mode((self.largura_tela,self.altura_tela))
         self.tela_atual=Tela_Jogo(self.window)
+        
     
     def game_loop(self):
         rodando=True 
