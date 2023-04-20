891e15f6982f3172d0543e0f44a6e28d93535fc1
laupontiroli <laurapm@al.insper.edu.br>
2023-04-20 11:54:12 -0300

    separei os assets das telas do jogo em arquivos diferentes

--- jogo/jogo_GM.py
+++ jogo/jogo_GM.py
@@ -1,43 +1,5 @@
 import pygame 
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self,window):
-        pygame.sprite.Sprite.__init__(self)
-        self.window=window
-        self.indice_img=0
-        self.images_animation=['jogo/Assets_jogo/Gingerman/gingerman_1.png','jogo/Assets_jogo/Gingerman/ginggerman_2.jpeg']
-        image=pygame.image.load(self.images_animation[self.indice_img])
-        self.image=pygame.transform.scale(image, (60,60))
-        self.vidas=2
-        self.h=self.image.get_height()
-        self.radius=(self.h)/2
-        self.last_updated=0
-        self.rect=self.image.get_rect()
-        self.rect.x=0
-        self.rect.y=540
-
-    def update(self):
-        self.indice_img=(self.indice_img+1)%len(self.images_animation)
-        image=pygame.image.load(self.images_animation[self.indice_img])
-        self.image=pygame.transform.scale(image, (60,60))
-    def desenha(self):
-        self.window.blit(self.image,self.rect)
-        pygame.display.update()
-
-class Biscoitos(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        pass
-
-class Nuvem(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-
-
-class Monstro(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-    
+from assets_jogo import * 
 
 class Tela_Jogo():
     
