5c28f54d6eaaf6c19054792441b7d0521993096e
Rafaela Afférri de Oliveira <rafaelaao@al.insper.edu.br>
2023-04-25 11:44:16 -0300

    comeco da tela inicial

--- jogo/jogo_GM.py
+++ jogo/jogo_GM.py
@@ -78,6 +78,63 @@ class Tela_Jogo():
         self.player.desenha()
         pygame.display.update()
 
+def checa_colisao(ret_x, ret_y, ret_largura, ret_altura, p_x, p_y):
+    if (
+        ret_x <= p_x and 
+        p_x <= ret_x + ret_largura and 
+        ret_y <= p_y and 
+        p_y <= ret_y + ret_altura
+    ):
+        return True
+    else:
+        return False
+
+class Tela_Inicial():
+    def __init__(self, jogo):
+        pygame.init()
+        self.window=jogo.window
+        self.largura_tela = jogo.largura_tela
+        self.altura_tela = jogo.altura_tela
+        fundo=pygame.image.load('jogo/Assets_jogo/img_fundo.png'). convert()
+        self.fundo=pygame.transform.scale(fundo, (800, 600))
+        self.tiles=math.ceil(self.window.get_width()/self.fundo.get_width())+1
+        self.scroll=0
+        self.clock=pygame.time.Clock()
+        self.FPS=15
+        self.fundo_jogar = pygame.Rect(self.largura_tela//2-150,self.altura_tela//2-100,300,100)
+        self.fonte_logo = pygame.font.Font("jogo/Assets_jogo/fontes/OnlineWebFonts_COM_2486b26012f1198dc8c84cbf5c960f98/Architype Aubette W90/Architype Aubette W90.ttf", 80)
+        self.jogar = False
+    def desenha(self):
+
+        if abs(self.scroll)>self.fundo.get_width():
+                self.scroll=0
+        else: 
+            self.scroll-=15
+        for i in range(self.tiles):
+            self.window.blit(self.fundo,(i*self.fundo.get_width()+self.scroll,0))
+        if self.jogar:
+            texto_jogar = self.fonte_logo.render("JOGAR",True,(184, 55, 38))
+            pygame.draw.rect(self.window, (240, 248, 255),self.fundo_jogar,0,15)
+        else:
+            texto_jogar = self.fonte_logo.render("JOGAR",True,(240, 248, 255))
+            pygame.draw.rect(self.window, (184, 55, 38),self.fundo_jogar,0,15)
+        self.window.blit(texto_jogar, (self.largura_tela//2-115,self.altura_tela//2-100))
+        pygame.display.update()
+    def atualiza(self):
+        self.clock.tick(self.FPS)
+        pos_mouse = pygame.mouse.get_pos()
+        if checa_colisao(self.fundo_jogar.x, self.fundo_jogar.y, self.fundo_jogar.width, self.fundo_jogar.height,pos_mouse[0],pos_mouse[1]):
+            self.jogar = True
+        else:
+            self.jogar= False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return -1
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                return Tela_Jogo(self.window)
+        return self
+
+
 
 class Jogo: 
     def __init__(self):
@@ -86,7 +143,9 @@ class Jogo:
         self.largura_tela=1300
         self.altura_tela=600
         self.window=pygame.display.set_mode((self.largura_tela,self.altura_tela))
-        self.tela_atual=Tela_Jogo(self.window)
+        self.tela_atual=Tela_Inicial(self)
+        
+        # self.tela_atual=Tela_Jogo(self.window)
         
     
     def game_loop(self):
