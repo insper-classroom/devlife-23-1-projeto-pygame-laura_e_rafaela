f9f5b2e2f182afed0c2baff88074855e91ceacf0
laupontiroli <laurapm@al.insper.edu.br>
2023-05-02 16:56:50 -0300

    documentação do arquivo

--- jogo/jogo_GM.py
+++ jogo/jogo_GM.py
@@ -4,8 +4,26 @@ import math
 import random
 
 class Tela_Jogo():
-    
+    ''' 
+    Essa classe é onde está tudo da tela do jogo
+    ...
+    Metodos(funções) da classe
+    ---------------------
+    atualiza:
+        atualiza o som, o estado da tela e todas as sprites 
+    desenha:
+        desenha o fundo da tela com movimento e desenha todas as sprites 
+    '''
+
     def __init__(self,window):
+        '''
+        Função que inicia a classe Tela_Jogo e define os valores do self
+        ...
+        Parâmetros:
+        -------
+        window:
+            janela do jogo 
+        '''
         self.window=window
         fundo=pygame.image.load('jogo/Assets_jogo/img_fundo.png'). convert()
         self.fundo=pygame.transform.scale(fundo, (800, 600))
@@ -13,11 +31,14 @@ class Tela_Jogo():
         self.FPS=15
         self.player=Player(self.window, self)
         self.all_monstros = pygame.sprite.Group()
+        #tiles e scroll são os responsáveis pela movimentação da tela, tiles é o num de telas necessário.
         self.tiles=math.ceil(self.window.get_width()/self.fundo.get_width())+1
         self.scroll=0
         self.andando=True
+        # dx é o ponto incial do x pra poder gerar sprites fora da tela 
         self.biscoito_dx = 300
         self.plat_dx=300
+        self.monstro_dx = 100
         self.all_biscoitos=pygame.sprite.Group()
         self.all_plataformas = pygame.sprite.Group()
         self.sound=pygame.mixer.Sound('jogo/Assets_jogo/jingle-bells-rock-energetic-positive-upbeat-happy-christmas-party-125676.mp3')
@@ -27,7 +48,6 @@ class Tela_Jogo():
         self.final_time=0
         self.start_time=pygame.time.get_ticks()
         self.last_updated=0
-        self.monstro_dx = 100
         for i in range(6):
             x = random.randint(500, 1000)
             width = random.randint(2, 6)
@@ -45,11 +65,17 @@ class Tela_Jogo():
 
 
     def atualiza(self):
+        '''
+        Função responsável por atualizar todas as sprites, verificar se os pontos foram atingidos, gerar
+        novas sprites e atualizar as telas. 
+        '''
         if not(self.musica_tocando):
             self.sound.play(-1)
             self.musica_tocando=True
         self.clock.tick(self.FPS)
         if not(self.pontos_atingidos):
+
+            #gerando sprites fora da janela mas sem criar um grupo muito grande de sprites desnecessariamente
             if len(self.all_plataformas)<100:
                 self.plat_dx+=1000
                 for i in range(10):
@@ -65,12 +91,12 @@ class Tela_Jogo():
                     x = random.randint(self.biscoito_dx+500,1500 + self.biscoito_dx)
                     biscoito=Biscoito(self.window,y,x)
                     self.all_biscoitos.add(biscoito)
-            
             if len(self.all_monstros)<100:
                 self.monstro_dx += 500
                 x=random.randint(self.monstro_dx,50+ self.monstro_dx)
                 monstro = Monstro(self.window, x)
                 self.all_monstros.add(monstro)
+
             player,pontos=self.player.update(self.all_biscoitos,self.all_plataformas,self.all_monstros,self.andando)
             if player == -1:
                 return -1
@@ -80,6 +106,7 @@ class Tela_Jogo():
             self.andando=player
             if pontos>500:
                 self.pontos_atingidos=True
+                #Apagar as sprites que ainda estão para vir para parar o mapa
                 for plataforma in self.all_plataformas:
                     if plataforma.rect.x>1300:
                         plataforma.kill()
@@ -92,11 +119,11 @@ class Tela_Jogo():
             
         else:
             passou=True
+            #verifica se todas as plataformas já passaram para finalizar o jogo
             for plataforma in self.all_plataformas:
                 if plataforma.rect.x>0:
                     passou=False
             self.andando=not(passou)
-            
             if not(self.andando) and self.player.rect.x<400:
                 self.casa.update()
             player,pontos=self.player.update(self.all_biscoitos,self.all_plataformas,self.all_monstros,self.andando)
@@ -105,11 +132,13 @@ class Tela_Jogo():
             if not player:
                 self.sound.stop()
                 return Tela_game_over(self.window)
+            
             if self.player.rect.x>=400:
                 self.sound.set_volume(0.25) 
                 temp=(pygame.time.get_ticks())-self.start_time
                 if self.final_time==0:
                     self.final_time=temp/1000
+                #espera um tempo antes de ir pra tela win pra dar tempo de ver a animação
                 if (temp/1000)-self.final_time>5:
                     self.sound.stop()
                     pontos+=(self.player.vidas)*50
@@ -117,6 +146,10 @@ class Tela_Jogo():
         return self
             
     def desenha(self):
+        '''
+        Função resposável por desenhar todo o cenário com movimento e chamar 
+        a função draw e desenha de todas as sprites
+        '''
         if self.andando:
             if abs(self.scroll)>self.fundo.get_width():
                 self.scroll=self.fundo.get_width()+self.scroll
@@ -133,15 +166,37 @@ class Tela_Jogo():
 
 
 class Tela_Inicial():
+    ''' 
+    Essa classe é onde está tudo da tela de inicio
+    ...
+    Metodos(funções) da classe
+    ---------------------
+    atualiza:
+        verifica cliques
+    desenha:
+        desenha os botões e movimenta o fundo
+    checa colisão:
+        verifica os cliques nos botões 
+    '''
     def __init__(self,window):
+        '''
+        Função que inicia a classe Tela_Inicial e define os valores do self
+        ...
+        Parâmetros:
+        -------
+        window:
+            janela do jogo 
+        '''
         pygame.init()
         self.window=window
         self.largura_tela =1300
         self.altura_tela =600
         fundo=pygame.image.load('jogo/Assets_jogo/img_fundo.png'). convert()
         self.fundo=pygame.transform.scale(fundo, (800, 600))
+        #tiles e scroll são os responsáveis pela movimentação da tela, tiles é o num de telas necessário.
         self.tiles=math.ceil(self.window.get_width()/self.fundo.get_width())+1
         self.scroll=0
+        
         self.clock=pygame.time.Clock()
         self.FPS=15
         self.fundo_jogar = pygame.Rect(self.largura_tela//2-150,self.altura_tela//2-100,300,100)
@@ -156,7 +211,10 @@ class Tela_Inicial():
         self.logo=pygame.image.load('jogo/Assets_jogo/cookie_chase_vermelho.png')
 
     def desenha(self):
-
+        '''
+        Função responsável por desenhar os botões e movimentar o fundo da tela.
+        Ela desenha os botões de cores diferentes se o mouse estiver em cima do botão
+        '''
         if abs(self.scroll)>self.fundo.get_width():
                 self.scroll=0
         else: 
@@ -187,7 +245,12 @@ class Tela_Inicial():
         self.window.blit(texto_regras, (self.largura_tela//2 - 140 ,self.altura_tela//2 + 40))
         self.window.blit(texto_jogar, (self.largura_tela//2-115,self.altura_tela//2-100))
         pygame.display.update()
+
     def atualiza(self):
+        '''
+        Verifica os cliques nos botões utilizando a função checa colisão retorna telas diferentes de 
+        acordo com os cliques 
+        '''
         self.clock.tick(self.FPS)
         pos_mouse = pygame.mouse.get_pos()
         if self.checa_colisao(self.fundo_jogar.x, self.fundo_jogar.y, self.fundo_jogar.width, self.fundo_jogar.height,pos_mouse[0],pos_mouse[1]):
@@ -216,6 +279,24 @@ class Tela_Inicial():
         return self
     
     def checa_colisao(self,ret_x, ret_y, ret_largura, ret_altura, p_x, p_y):
+        '''
+        Função responsável por chegar se o jogador clicou nos botões:
+
+        Parâmetros:
+        ----------
+        ret_x:
+            x do retângulo do botão
+        ret_y:
+            y do retângulo do botão
+        ret_largura:
+            largura do retângulo do botão 
+        ret_altura:
+            altura do retângulo do botão 
+        p_x:
+            x da posição do mouse 
+        p_y:
+            y da posição do mouse
+        '''
         if (
             ret_x <= p_x and 
             p_x <= ret_x + ret_largura and 
@@ -229,6 +310,14 @@ class Tela_Inicial():
 
 class Tela_Regras ():
     def __init__(self,window):
+        '''
+        Função que inicia a classe Tela_Regras e define os valores do self
+        ...
+        Parâmetros:
+        -------
+        window:
+            janela do jogo 
+        '''
         pygame.init()
         self.window=window
         fundo=pygame.image.load('jogo/Assets_jogo/img_fundo_regras.png').convert()
@@ -243,6 +332,10 @@ class Tela_Regras ():
         self.clock=pygame.time.Clock()
 
     def desenha(self):
+        '''
+        Função responsável por desenhar os botões e o fundo da tela.
+        Ela desenha os botões de cores diferentes se o mouse estiver em cima do botão
+        '''
         self.window.blit(self.fundo,(0,0))
         if self.jogar:
             texto_jogar = self.fonte_jogar.render("JOGAR",True,(184, 55, 38))
@@ -262,6 +355,10 @@ class Tela_Regras ():
 
 
     def atualiza(self):
+        '''
+        Função responsável por verificar os cliques na tela e atualiza-la de acordo com o clique 
+        fazendo a mudança entre telas
+        '''
         self.clock.tick(self.FPS)
         pos_mouse = pygame.mouse.get_pos()
         if self.checa_colisao(self.fundo_jogar.x, self.fundo_jogar.y, self.fundo_jogar.width, self.fundo_jogar.height,pos_mouse[0],pos_mouse[1]):
@@ -283,6 +380,24 @@ class Tela_Regras ():
         return self
 
     def checa_colisao(self,ret_x, ret_y, ret_largura, ret_altura, p_x, p_y):
+        '''
+        Função responsável por chegar se o jogador clicou nos botões:
+
+        Parâmetros:
+        ----------
+        ret_x:
+            x do retângulo do botão
+        ret_y:
+            y do retângulo do botão
+        ret_largura:
+            largura do retângulo do botão 
+        ret_altura:
+            altura do retângulo do botão 
+        p_x:
+            x da posição do mouse 
+        p_y:
+            y da posição do mouse
+        '''
         if (
             ret_x <= p_x and 
             p_x <= ret_x + ret_largura and 
@@ -295,6 +410,14 @@ class Tela_Regras ():
 
 class Tela_game_over():
     def __init__(self,window):
+        '''
+        Função que inicia a classe Tela_game_over e define os valores do self
+        ...
+        Parâmetros:
+        -------
+        window:
+            janela do jogo 
+        '''
         pygame.init()
         self.window=window
         self.largura_tela =1300
@@ -306,7 +429,12 @@ class Tela_game_over():
         self.jogar_dnv=False
         self.som=pygame.mixer.Sound('jogo/Assets_jogo/game_over.wav')
         self.som_tocou=False
+
     def desenha(self):
+        '''
+        Função responsável por desenhar os botões e o fundo da tela.
+        Ela desenha os botões de cores diferentes se o mouse estiver em cima do botão
+        '''
         self.window.blit(self.fundo,(0,0))
         if self.jogar_dnv:
             texto_jogar_dnv = self.fonte_jogar_dnv.render("JOGAR NOVAMENTE",True,(184, 55, 38))
@@ -317,7 +445,12 @@ class Tela_game_over():
         self.window.blit(texto_jogar_dnv, (self.largura_tela//2-325,self.altura_tela//2-50))
         pygame.display.update()
 
+
     def atualiza(self):
+        '''
+        toca o som da tela e verifica os cliques na tela.
+        Muda de tela de acordo com o clique
+        '''
         if not(self.som_tocou):
             self.som.play()
             self.som_tocou=True
@@ -336,6 +469,24 @@ class Tela_game_over():
         return self
     
     def checa_colisao(self,ret_x, ret_y, ret_largura, ret_altura, p_x, p_y):
+        '''
+        Função responsável por chegar se o jogador clicou nos botões:
+
+        Parâmetros:
+        ----------
+        ret_x:
+            x do retângulo do botão
+        ret_y:
+            y do retângulo do botão
+        ret_largura:
+            largura do retângulo do botão 
+        ret_altura:
+            altura do retângulo do botão 
+        p_x:
+            x da posição do mouse 
+        p_y:
+            y da posição do mouse
+        '''
         if (
             ret_x <= p_x and 
             p_x <= ret_x + ret_largura and 
@@ -348,6 +499,14 @@ class Tela_game_over():
 
 class Tela_Info ():
     def __init__(self,window):
+        '''
+        Função que inicia a classe Tela_Info e define os valores do self
+        ...
+        Parâmetros:
+        -------
+        window:
+            janela do jogo 
+        '''
         pygame.init()
         self.window=window
         fundo=pygame.image.load('jogo/Assets_jogo/fundo_tela_info.png').convert()
@@ -362,6 +521,10 @@ class Tela_Info ():
         self.clock=pygame.time.Clock()
 
     def desenha(self):
+        '''
+        Função responsável por desenhar os botões e o fundo da tela.
+        Ela desenha os botões de cores diferentes se o mouse estiver em cima do botão
+        '''
         self.window.blit(self.fundo,(0,0))
         if self.jogar:
             texto_jogar = self.fonte_jogar.render("JOGAR",True,(184, 55, 38))
@@ -380,6 +543,9 @@ class Tela_Info ():
         pygame.display.update()
 
     def atualiza(self):
+        '''
+        Função responsável por verificar os cliques na tela e mudar as telas
+        '''
         self.clock.tick(self.FPS)
         pos_mouse = pygame.mouse.get_pos()
         if self.checa_colisao(self.fundo_jogar.x, self.fundo_jogar.y, self.fundo_jogar.width, self.fundo_jogar.height,pos_mouse[0],pos_mouse[1]):
@@ -401,6 +567,24 @@ class Tela_Info ():
         return self
 
     def checa_colisao(self,ret_x, ret_y, ret_largura, ret_altura, p_x, p_y):
+        '''
+        Função responsável por chegar se o jogador clicou nos botões:
+
+        Parâmetros:
+        ----------
+        ret_x:
+            x do retângulo do botão
+        ret_y:
+            y do retângulo do botão
+        ret_largura:
+            largura do retângulo do botão 
+        ret_altura:
+            altura do retângulo do botão 
+        p_x:
+            x da posição do mouse 
+        p_y:
+            y da posição do mouse
+        '''
         if (
             ret_x <= p_x and 
             p_x <= ret_x + ret_largura and 
@@ -414,6 +598,18 @@ class Tela_Info ():
 
 class Tela_win():
     def __init__(self,window,pontos,tempo):
+        '''
+        Função que inicia a classe Tela_win e define os valores do self
+        ...
+        Parâmetros:
+        -------
+        window:
+            janela do jogo 
+        pontos:
+            pontos finais do jogador 
+        tempo:
+            tempo que o jogador precisou pra ganhar o jogo
+        '''
         pygame.init()
         self.window=window
         self.largura_tela =1300
@@ -430,6 +626,10 @@ class Tela_win():
         self.som_tocou=False
 
     def desenha(self):
+        '''
+        Função responsável por desenhar o botão, o fundo da tela, os pontos e o tempo.
+        Ela desenha os botões de cores diferentes se o mouse estiver em cima do botão
+        '''
         self.window.blit(self.fundo,(0,0))
         if self.jogar_dnv:
             texto_jogar_dnv = self.fonte_jogar_dnv.render("JOGAR NOVAMENTE",True,(184, 55, 38))
@@ -446,6 +646,9 @@ class Tela_win():
         pygame.display.update()
 
     def atualiza(self):
+        '''
+        Função que toca o som da vitória, verifica o clique e atualiza a tela
+        '''
         if not(self.som_tocou):
             self.som_tocou=True
             self.sound.play()
@@ -463,6 +666,24 @@ class Tela_win():
         return self
     
     def checa_colisao(self,ret_x, ret_y, ret_largura, ret_altura, p_x, p_y):
+        '''
+        Função responsável por chegar se o jogador clicou nos botões:
+
+        Parâmetros:
+        ----------
+        ret_x:
+            x do retângulo do botão
+        ret_y:
+            y do retângulo do botão
+        ret_largura:
+            largura do retângulo do botão 
+        ret_altura:
+            altura do retângulo do botão 
+        p_x:
+            x da posição do mouse 
+        p_y:
+            y da posição do mouse
+        '''
         if (
             ret_x <= p_x and 
             p_x <= ret_x + ret_largura and 
@@ -474,7 +695,18 @@ class Tela_win():
             return False
 
 class Jogo: 
+    '''
+    Classe reponsável por criar a janela (window) do jogo e ter o game_loop do jogo 
+
+    Métodos (funções) da classe:
+    -------------------------
+    game_loop:
+        loop principal do jogo 
+    '''
     def __init__(self):
+        '''
+        função que inicializa o jogo criando a janela
+        '''
         pygame.init()
         pygame.display.set_caption('Cookie Chase')
         self.largura_tela=1300
@@ -485,6 +717,9 @@ class Jogo:
         
     
     def game_loop(self):
+        '''
+        Função responsável pelo game loop principal do jogo fazendo a mudança entre telas e desenhando as telas
+        '''
         rodando=True 
         while rodando:
             self.tela_atual=self.tela_atual.atualiza()
